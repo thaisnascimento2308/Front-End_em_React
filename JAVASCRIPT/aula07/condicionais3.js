0e699234d3223457f88a22096889e577911bcf84
Thais <thaisnascimento2308@gmail.com>
2025-05-10 15:59:42 -0300

    Condicionais 3

---
+++ JAVASCRIPT/aula07/condicionais3.js
@@ -0,0 +1,12 @@
+//*Operador Ternário
+const idade = 20;
+
+// idade >= 18 ? console.log("Maior de idade") : console.log("Menor de idade");
+
+const mensagem = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+console.log(mensagem);
+
+//*Truthy e Falsy
+//*Falsy: 0, undefined, null, ' ', NaN
+//*Truthy: Todos os demais valores.
