48ff8b545eef8c7de5c36816238139ce58b4ffdd
Thais <thaisnascimento2308@gmail.com>
2025-05-27 17:52:34 -0300

    Funções alta ordem

---
+++ JAVASCRIPT/aula11/funcoes-alta-ordem.js
@@ -0,0 +1,35 @@
+// HIGH ORDER FUNCTIONS
+
+// * 1. Função que RETORNA outra função como parâmetro
+
+function welcome(courseName) {
+    return (studentName) => {
+        console.log(
+            `Ólá, ${studentName}! Seja bem-vinda(o) ao curso de ${courseName}!`
+            );
+    };
+}
+
+const displayWelcomeToFrontEndCourse = welcome("Front-end em React");
+const displayWelcomeToBackEndCourse = welcome("Back-end em Node.js");
+
+displayWelcomeToFrontEndCourse("Thais Nascimento");
+
+console.clear()
+
+// * 2. Função que RECEBE outra função como parâmetro
+
+const somar = (num1, num2) => num1 + num2;
+const subtrair = (num1, num2) => num1 - num2;
+const multiplicar = (num1, num2) => num1 * num2;
+const dividir = (num1, num2) => num1 / num2;
+
+// funcaoDaOperação: função que realiza a operação entre os dois números, ou eja, ela precisa ser uma função que recebe dois números.
+const calcular = (num1, num2,  funcaoDaOperacao) => funcaoDaOperacao(num1, num2);
+
+//Calcular = 10, 5, somar) => somar(10, 5)
+
+const retorno = calcular(10, 5, multiplicar);
+
+console.log(retorno);
+
