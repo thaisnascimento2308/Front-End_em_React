343f06d66625da35952a685538e1fcc7defaa166
Thais <thaisnascimento2308@gmail.com>
2026-01-07 18:47:32 -0300

    Continua Projeto todo-list

--- JAVASCRIPT/aula28/todo-list/src/components/Header/Header.tsx
+++ JAVASCRIPT/aula28/todo-list/src/components/Header/Header.tsx
@@ -1,4 +1,7 @@
+// Importa o React para usar JSX e definir componentes funcionais
+import React from 'react';
 
+// Define e exporta o componente funcional Header
 export const Header:React.FC = () => { // Define o componente funcional Header
   return <h1>Header</h1>
 }
