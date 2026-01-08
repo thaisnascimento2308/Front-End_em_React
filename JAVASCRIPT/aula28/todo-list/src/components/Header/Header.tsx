8d08452e3cb2b0e1e4542a9192f3be0b3e5f8a14
Thais <thaisnascimento2308@gmail.com>
2026-01-08 19:03:47 -0300

    Adiciona styles, cards e props.

--- JAVASCRIPT/aula28/todo-list/src/components/Header/Header.tsx
+++ JAVASCRIPT/aula28/todo-list/src/components/Header/Header.tsx
@@ -1,16 +1,22 @@
 
-import "./styles.css";
+import { StatsCard } from "../StatsCard/StatsCard";
+import styles from "./styles.module.scss";
 // Define e exporta o componente funcional Header
 export const Header:React.FC = () => { // Define o componente funcional Header
   return (
-    <header className="header">
-      <div>
-        <h1>MyTodo</h1>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>MyTodo</h1>
 
-        <span>Bem-vinda, Thais.</span>
+          <span>Bem-vinda, Thais Nascimento!</span>
+        </div>
+        <div>
+          <StatsCard title="Total de Tarefas" value={5} />
+          <StatsCard title="Tarefas Pendentes" value={4} />
+          <StatsCard title="Tarefas Concluídas" value={1} />
+        </div>
       </div>
-
-      <div>{/*Cards*/}</div>
     </header>
   )
 };
